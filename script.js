efd7a20b28cf573ba454cbb576c8e2e8535f91b4
Arnold Cubici-Jones <arnoldcubicijones@hotmail.co.uk>
2022-10-03 17:25:46 +0100

    Define grid by squaresPerSide

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //variables
-var xAxis = null;
-var yAxis = null;
-var totalDivs = xAxis * yAxis;
+var squaresPerSide = null;
+var totalDivs = squaresPerSide * squaresPerSide;
 
 // functions
 function eventHandler(pixelNumber) { /* colorChanger is nested inside eventHandler to be able to accept the argument x, the pixel number. */
@@ -13,38 +12,37 @@ console.log("eventHandler running");
    
 }
 function sizer() { // Runs at page start, and onclick with Grid Size button. changes the sketch grid size.
-for(let x = 0; x < totalDivs; x++) { //removes old divs
-document.getElementById('housingDiv').removeChild(document.getElementById(x));
-}
-xAxis = prompt('Enter the grid width: ', 'Between 1-16');
-yAxis = prompt('Enter the grid height: ', 'Between 1-16');
-totalDivs = xAxis*yAxis; // gets new gridsize num
+    for(let x = 0; x < totalDivs; x++) { //removes old divs
+        document.getElementById('housingDiv').removeChild(document.getElementById(x));
+    }
+squaresPerSide = prompt('Enter a number between 1-100 to dictate the squares per axis: ', '1-100');
 
+totalDivs = squaresPerSide*squaresPerSide // gets new gridsize num
 var housingDiv = document.getElementById('housingDiv');
 
-for (let x = 0; x < (totalDivs); x++) {
-    var gridDiv = document.createElement('div');
-    gridDiv.id = x;
-    gridDiv.classList.add('gridDiv');
-    gridDiv.addEventListener('mouseover', eventHandler(gridDiv.id));
-    document.getElementById('housingDiv').appendChild(gridDiv);
-}    
+
+
+
+
+
+
+
+
+    for (let x = 0; x < (totalDivs); x++) {
+        var gridDiv = document.createElement('div');
+        gridDiv.id = x;
+        gridDiv.classList.add('gridDiv');
+        gridDiv.addEventListener('mouseover', eventHandler(gridDiv.id));
+        document.getElementById('housingDiv').appendChild(gridDiv);
+    }    
 return;
 }
     sizer(); //runs sizer to establish init size of grid
-
-/*
-
-1. change markup to adapt individual boxes to grid full size.
-
-*/
-
-function resetFunct() {
-console.log('resetfunct');
-totalDivs = xAxis*yAxis;
-    for (x=0; x<totalDivs; x++) {
+function resetFunct() { //removes class from all divs within the grid
+    totalDivs = squaresPerSide*squaresPerSide;
+        for (x=0; x<totalDivs; x++) {
             document.getElementById(x).classList.remove('filledInClass');
-    }
+        }
 }
 
 // EVENT LISTENERS
