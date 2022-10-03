33a3959b6ca25cc180ebf0c6e82142f81f2e6126
Arnold Cubici-Jones <arnoldcubicijones@hotmail.co.uk>
2022-10-03 15:58:37 +0100

    Make colorChanger fnctn work regardless of sizer

--- script.js
+++ script.js
@@ -1,74 +1,57 @@
-//Variables
-
-var toAdd = document.createElement('div');
+//variables
 var xAxis = null;
 var yAxis = null;
-var totalDivs = 256;
-
-if (xAxis || yAxis == null) {
-totalDivs == 256;
-}
+var totalDivs = xAxis * yAxis;
 
 // functions
-function eventHandler(x) { /* colorChanger is nested inside eventHandler to be able to accept the argument x, the pixel number. */
-var pixelNum = x;
+function eventHandler(pixelNumber) { /* colorChanger is nested inside eventHandler to be able to accept the argument x, the pixel number. */
 console.log("eventHandler running");
     return function colorChanger () { // executed if the event occurs
-    document.getElementById(x).setAttribute('style', 'background-color: rgb(255, 204, 0)');
-    console.log("colorChanger running");
+    document.getElementById(pixelNumber).classList.add('filledInClass');
+    console.log('colorChanger running');
     }
    
 }
+function sizer() { // Runs at page start, and onclick with Grid Size button. changes the sketch grid size.
+for(let x = 0; x < totalDivs; x++) { //removes old divs
+document.getElementById('housingDiv').removeChild(document.getElementById(x));
+}
+xAxis = prompt('Enter the grid width: ', 'Between 1-16');
+yAxis = prompt('Enter the grid height: ', 'Between 1-16');
+totalDivs = xAxis*yAxis; // gets new gridsize num
 
-function sizer() { // changes the size of the grid
-var xAxis = prompt('Enter the grid width: ', 'Between 1-16');
-var yAxis = prompt('Enter the grid height: ', 'Between 1-16');
-totalDivs = xAxis * yAxis;
-var newToAdd = document.createElement('div');
-    newToAdd.id = "gridDiv"
-    for (let x = 0; x < totalDivs; x++) {
-        var inDiv = document.createElement('div');
-        inDiv.id = x;
-        inDiv.className = 'pixel';
-        newToAdd.appendChild(inDiv);
-    }
-document.getElementById('housingDiv').appendChild(newToAdd);    
+var housingDiv = document.getElementById('housingDiv');
+
+for (let x = 0; x < (totalDivs); x++) {
+    var gridDiv = document.createElement('div');
+    gridDiv.id = x;
+    gridDiv.classList.add('gridDiv');
+    gridDiv.addEventListener('mouseover', eventHandler(gridDiv.id));
+    document.getElementById('housingDiv').appendChild(gridDiv);
+}    
 return;
 }
     sizer(); //runs sizer to establish init size of grid
 
 /*
 
-at the moment i can re-create the size of the grid
-
-it will create boxes to the number of height times width
-
-i need those to be ordered into 100% of the bigger box - this involves a changed markup - i need a certain number of heightXwidth which is hard when grid created via numbers not table
-
-
-i also need the colorchange function to work whenever the size changes**
-
-
-
 1. change markup to adapt individual boxes to grid full size.
 
-2. change color funct to work regardless of gridsize
+
 
 
 problem im trying to solve is twofold. 
-1) how to change grid size and colorchanger function still works. when you initially run gridchanger function on page startup, it works! but when you change it again it doesn't.
+1) when you initially run sizer function on page startup, colorChanger funct works! but when you run sizer again on click colorchanger doesn't work after that. the basic CSS does work, and hover will change bckgrnd cl.
+
 2) how to change grid size and markup shows grid as a box rather than a barcode.
 
 */
 
 function resetFunct() {
-if (xAxis || yAxis == null) {
-    totalDivs = 256;
-} else {
-    totalDivs = xAxis * yAxis;
-}
+console.log('resetfunct');
+totalDivs = xAxis*yAxis;
     for (x=0; x<totalDivs; x++) {
-            document.getElementById(x).setAttribute('style', 'background-color: cornflowerblue');
+            document.getElementById(x).classList.remove('filledInClass');
     }
 }
 
@@ -76,33 +59,5 @@ if (xAxis || yAxis == null) {
 
 // Reset listener
 document.getElementById('reset').addEventListener('click', resetFunct);
-//Color Changer
-for (x=0; x<totalDivs; x++) {
-    document.getElementById(x).addEventListener('mouseout', eventHandler(x));
-}
-
-document.getElementById('gridSize').addEventListener('click', sizer)
-
-function testFunction() {
-    return document.getElementById('30').setAttribute('style', 'background-color: red');
-}
-/*
-
-
-Add gridSize function to change size on a prompt
-
-prompt called on gridSize click
-prompt opens, has height and width textbox
-information gets put into function
-
-table instead of divs to eliminate need to style
-grid.
-
-either max amount of elements per width/height changes and boxes fluid size
-or num changes with sizing formula per num of boxes
-
-either way, with gridSize being changed, the size of the boxes will change. i want the size of the main housingDiv to remain static.
-
-
-
-*/
+//grid sizer
+document.getElementById('gridSize').addEventListener('click', sizer);
